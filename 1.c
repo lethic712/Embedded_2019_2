c08fd652eb3b74939b1997007e3f63f79f6ad3df
donghyun <dong2681@maj.ac.kr>
2019-10-28 10:07:42 +0700

    1.c -2
    1.c 2

--- 1.c
+++ 1.c
@@ -1,12 +1,10 @@
-#include <stido.h>
+#include “myProject.h"
 
-int main(void)
+int printMyName1(void) //1~4중 하나. 파일이름에 맞게 할 것
 {
-
-printf("hello! \n");
-
+printf (“my name is [kimdonghyun]\n”); //자기 이름
+return 1;
 }
-
 	
 	
 	

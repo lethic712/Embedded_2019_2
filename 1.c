8033e0ff51ab0ce3252ba48f53a042617e43743d
Seungryeol <lethic712@mju.ac.kr$>
2019-10-28 12:16:14 +0900

    1.c
    1.c

--- 1.c
+++ 1.c
@@ -1,8 +1,8 @@
-#include “myProject.h"
+#include "myProject.h"
 
 int printMyName1(void) //1~4중 하나. 파일이름에 맞게 할 것
 {
-printf (“my name is [kimdonghyun]\n”); //자기 이름
+printf ("my name is [kimdonghyun]\n”); //자기 이름
 return 1;
 }
 	

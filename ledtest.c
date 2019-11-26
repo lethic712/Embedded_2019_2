11c9f63179f187f9f7b26486cd2321d5d56c6668
Seungryeol <lethic712@mju.ac.kr$>
2019-11-26 15:57:55 +0900

    ledtest

--- ledtest.c
+++ ledtest.c
@@ -8,39 +8,46 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#define LED_DRIVER_NAME "/dev/periled"
+#include <math.h>
 #include "led.h"
 
 void doHelp(void)
 {
-printf("ledtest <hex byte> :data bit0 operation 1=>on 0=>off\n");
-printf(" ledtest 0x05 ;4th and 1th led on\n");
-printf(" ledtest 0xff ;all led on\n");
-printf(" ledtest 0x00 ;all led off\n");
+	printf("ledtest <hex byte> :data bit0 operation 1=>on 0=>off\n");
+	printf(" ledtest 0x05 ;3th and 1th led on\n");
+	printf(" ledtest 0xff ;all led on\n");
+	printf(" ledtest 0x00 ;all led off\n");
 }
 
 int main(int argc , char **argv)
 {
-unsigned int data = 0;
-int fd;
-if (argc < 2 )
-{
-perror(" Args number is less than 2\n");
-doHelp();
-return 1;
-}
-data = strtol(argv[1],NULL,16); //String을 16진수로 가정하고 integer형으로 변환
-printf("wrate data :0x%X\n", data);
+	unsigned int data = 0;
+	int i;
+	int tem1 = 0, tem2 = 0, tem3 = 0;
+	
+	if (argc < 2 )
+	{
+		perror(" Args number is less than 2\n");
+		doHelp();
+		return 1;
+	}
+	
+	data = strtol(argv[1],NULL,16); //String을 16진수로 가정하고 integer형으로 변환
+	printf("wrate data :0x%X\n", data);
 
-// open driver
-fd = open(LED_DRIVER_NAME,O_RDWR);
-if ( fd < 0 )
-{
-perror("driver (//dev//cnled) open error.\n");
-return 1;
-}
-
-write(fd,&data,4);
-close(fd);
+	// open driver
+	ledLibInit();
+	
+	tem1 = data;
+	
+	for(i = 0; i < 8; i++){
+		if((tem1 % 2) == 1){
+			ledOnOff (i, 1);
+		}
+		tem1 = tem1 / 2; 
+	}
+	
+	ledLibExit();
+	
 return 0;
 }
